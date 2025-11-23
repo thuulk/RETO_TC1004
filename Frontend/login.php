131ce54098cdba1052b8889c0a722d867be5a765
AngelTorres321 <a00228324@tec.mx>
2025-11-22 20:34:25 -0700

    Update login.php

--- Frontend/login.php
+++ Frontend/login.php
@@ -1,15 +1,22 @@
 <?php
 session_start();
-
-$user = "admin";
-$pass = "1234";
+include "conexion.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    if ($username === $user && $password === $pass) {
+    $sql = "SELECT * FROM usuarios WHERE matricula = ?";
+    $stmt = mysqli_prepare($conn,$sql);
+    mysqli_stmt_bind_param($stmt, 's', $username);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $row = mysqli_fetch_assoc($result);
+
+    if ($username === $row["matricula"] && $password === $row["contraseña"]) {
         $_SESSION["username"] = $username;
+        $_SESSION["contraseña"] = $password;
+        $_SESSION["rol"] = $row["rol"];
         header("Location: index.php");
         exit();
     } else {
