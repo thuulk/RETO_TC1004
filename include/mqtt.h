d7e89dcf431731f9cbb33101851396ad6f724c2f
Diego <diegoreyesbarraza@hotmail.com>
2025-11-24 14:58:01 -0700

    Remove sensitive include files from Git and apply .gitignore

--- include/mqtt.h
+++
@@ -1,6 +0,0 @@
-#ifndef FIRMWARE_INCLUDE_MQTT_H
-#define FIRMWARE_INCLUDE_MQTT_H
-#define MQTT_SERVER "172.20.10.2"
-#define MQTT_PORT 1883
-#define TOPIC "sensores/diego"
-#endif
--- include/wifi.h
+++
@@ -1,5 +0,0 @@
-#ifndef FIRMWARE_INLCUDE_WIFI_H
-#define FIRMWARE_INCLUDE_WIFI_H
-#define WIFI_SSID = ""
-#define WIFI_PASSWORD = ""
-#endif
